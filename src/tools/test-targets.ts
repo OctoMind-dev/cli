729bf01c0459e6591d0b03971594acb160efb6a9
mxlnk <maxi.link@octomind.dev>
2025-07-25 14:38:25 +0200

    add list test targets (#176)
    * add list test targets
    
    * list test targets and allow switching test targets
    
    * fix schema
    
    ---------
    
    Co-authored-by: Stefan Rinke <sker65@gmail.com>

---
+++ src/tools/test-targets.ts
@@ -0,0 +1,32 @@
+import { client, handleError, ListOptions, logJson } from "./client";
+
+export const getTestTargets = async () => {
+  const { data, error } = await client.GET("/apiKey/v2/test-targets");
+
+  handleError(error);
+
+  if (!data) {
+    throw Error("No test targets found");
+  }
+
+  return data;
+};
+
+export const listTestTargets = async (options: ListOptions): Promise<void> => {
+  const testTargets = await getTestTargets();
+
+  if (options.json) {
+    logJson(testTargets);
+    return;
+  }
+
+  console.log("Test Targets:");
+  testTargets.forEach((testTarget, idx) => {
+    const idxString = `${idx + 1}. `.padEnd(
+      testTargets.length.toString().length + 2,
+    );
+    const paddingString = " ".repeat(idxString.length);
+    console.log(`${idxString}ID: ${testTarget.id}`);
+    console.log(`${paddingString}App: ${testTarget.app}`);
+  });
+};
