7958cc4d3718ef9c1e59548997ffd55797f3f618
Maximilian Link <maxi.link@octomind.dev>
2025-07-25 11:14:39 +0200

    list test cases

--- src/tools/playwright.ts
+++ src/tools/playwright.ts
@@ -1,4 +1,4 @@
-import { client, handleError, outputResult } from "./client";
+import { client, handleError, logJson } from "./client";
 
 export const getPlaywrightConfig = async (options: {
   testTargetId: string;
@@ -6,7 +6,6 @@ export const getPlaywrightConfig = async (options: {
   url: string;
   outputDir: string;
   headless?: boolean;
-  json?: boolean;
 }): Promise<string> => {
   const { data, error } = await client.GET(
     "/apiKey/v2/test-targets/{testTargetId}/config",
@@ -32,10 +31,6 @@ export const getPlaywrightConfig = async (options: {
     throw new Error("no config found");
   }
 
-  if (options.json) {
-    outputResult(data);
-  }
-
   return data;
 };
 
@@ -44,7 +39,6 @@ export const getPlaywrightCode = async (options: {
   testCaseId: string;
   environmentId?: string;
   executionUrl: string;
-  json?: boolean;
 }): Promise<string> => {
   const { data, error } = await client.GET(
     "/apiKey/v2/test-targets/{testTargetId}/test-cases/{testCaseId}/code",
@@ -65,12 +59,7 @@ export const getPlaywrightCode = async (options: {
   handleError(error);
 
   if (!data) {
-    console.log({ data, error });
     throw new Error("no test code found");
-  }
-
-  if (options.json) {
-    outputResult(data);
   }
 
   return data.testCode;
