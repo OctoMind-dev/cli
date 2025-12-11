5f92654a5a44bdf400347f56714e7aa422a26299
Daniel Draper <daniel.draper@octomind.dev>
2025-12-11 19:21:23 +0100

    fix formatting

--- orval.config.ts
+++ orval.config.ts
@@ -1,10 +1,6 @@
-
-
-import { defineConfig } from 'orval';
-
+import {defineConfig} from 'orval';
 
 export default defineConfig({
-
     'octomind': {
         input: {
             target: "https://app.octomind.dev/openapi.yaml",
