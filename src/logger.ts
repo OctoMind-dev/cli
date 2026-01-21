8aa77724f3bbc7e103fb6d8457aed994439eee7b
Daniel Draper <daniel.draper@octomind.dev>
2026-01-21 13:14:37 +0100

    review

--- src/logger.ts
+++ src/logger.ts
@@ -10,14 +10,13 @@ import {
 export const logger = getLogger("octomind");
 
 const ansiColors = {
-  black: "\x1B[30m",
   red: "\x1B[31m",
   green: "\x1B[32m",
   yellow: "\x1B[33m",
   blue: "\x1B[34m",
   magenta: "\x1B[35m",
   cyan: "\x1B[36m",
-  white: "\x1B[37m",
+  // use native terminal colors
   none: "",
 };
 
