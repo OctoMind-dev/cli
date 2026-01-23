9d0ab94734c062fc1dc38d9d5e10eedbba877b81
Stefan Rinke <sker65@gmail.com>
2026-01-23 08:56:20 +0100

    adjust route

--- src/tools/test-plans.ts
+++ src/tools/test-plans.ts
@@ -3,18 +3,22 @@ import { client, handleError } from "./client";
 
 export type TestPlanResponse = components["schemas"]["ExternalTestPlanSchema"];
 export type GetTestPlanParams =
-  paths["/apiKey/beta/test-plans/{id}"]["get"]["parameters"]["path"];
+  paths["/apiKey/beta/test-targets/{testTargetId}/test-plans/{id}"]["get"]["parameters"]["path"];
 
 export const getTestPlan = async (
   options: GetTestPlanParams,
 ): Promise<TestPlanResponse> => {
-  const { data, error } = await client.GET("/apiKey/beta/test-plans/{id}", {
-    params: {
-      path: {
-        id: options.id,
+  const { data, error } = await client.GET(
+    "/apiKey/beta/test-targets/{testTargetId}/test-plans/{id}",
+    {
+      params: {
+        path: {
+          id: options.id,
+          testTargetId: options.testTargetId,
+        },
       },
     },
-  });
+  );
 
   handleError(error);
 
